2045352175bd7fc07aeec9d0add3870d244f56c9
Danil <mr.danechek0@gmail.com>
2021-02-10 00:34:22 +0200

    init commit

---
+++ app/src/main/java/ua/kpi/comsys/io8324/MainActivity.java
@@ -0,0 +1,13 @@
+package ua.kpi.comsys.io8324;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
