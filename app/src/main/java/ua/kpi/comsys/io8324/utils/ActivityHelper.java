e505af2e6e1eb3e022fc5497aab18fad676fa967
Danil Solovjov <47897059+danilos1@users.noreply.github.com>
2021-05-05 00:19:09 +0300

    refactored view initialization (#9)

--- app/src/main/java/ua/kpi/comsys/io8324/utils/ActivityHelper.java
+++ app/src/main/java/ua/kpi/comsys/io8324/utils/ActivityHelper.java
@@ -3,12 +3,17 @@ package ua.kpi.comsys.io8324.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+
+import ua.kpi.comsys.io8324.R;
 
 
 public class ActivityHelper {
+    public static final String TAG = "ActivityHelper";
 
     private ActivityHelper() {
     }
@@ -21,5 +26,18 @@ public class ActivityHelper {
         return activity.getResources()
                 .getConfiguration()
                 .orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+
+    public static int getResourceByString(String resourceName) {
+        try {
+            Field field = R.id.class.getDeclaredField(resourceName);
+            return field.getInt(field);
+        } catch (NoSuchFieldException e) {
+            Log.e(TAG, "error at "+e.getMessage());
+            return -1;
+        } catch (IllegalAccessException e) {
+            Log.e(TAG, "error at "+e.getMessage());
+            return -1;
+        }
     }
 }
